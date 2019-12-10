c08915f6050edade87e8773d6ba32baeca2cc5c9
Oleg Gavrilin <tomnium.gavrilin@gmail.com>
2019-12-10 13:11:33 +0200

    store init wip

---
+++ src/redux/reducers/budget/index.js
@@ -0,0 +1,10 @@
+const initialState ={
+    totalBudget:0,
+    monthsBudget:[]
+};
+
+const budget = (state = {}, action) => {
+    return state
+};
+
+export default budget
---
+++ src/redux/reducers/waste/index.js
@@ -0,0 +1,5 @@
+const waste = (state = {}, action) => {
+    return state
+};
+
+export default waste
